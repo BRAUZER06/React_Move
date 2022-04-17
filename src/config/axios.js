2e11e2aca12d3310d0aad6cc8c56063ab5f88470
AHMATOLOG <murtaz.oziev@mail.ru>
2022-04-17 12:25:35 +0300

    коммит перед тем как сделать 1 экшен на 3 категории

--- src/config/axios.js
+++ src/config/axios.js
@@ -18,4 +18,28 @@ export const instanceV2_1 = axios.create({
   baseURL: "https://kinopoiskapiunofficial.tech/api/v2.1",
 });
 
+export const instance_FilmsAndSeries = axios.create({
+  headers: {
+    "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86",
+    "Content-Type": "application/json",
+  },
+  baseURL:
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&",
+});
 
+export const instance_Trailer = axios.create({
+  headers: {
+    "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86",
+    "Content-Type": "application/json",
+  },
+  baseURL:
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&",
+});
+export const instance_Premiere = axios.create({
+  headers: {
+    "X-API-KEY": "8c8e1a50-6322-4135-8875-5d40a5420d86",
+    "Content-Type": "application/json",
+  },
+  baseURL:
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page",
+});
